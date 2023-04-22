88daf4b97a552e6552dcac171ef3b8127b3bf759
Gunjan Jain <jain10gunjan@gmail.com>
2023-04-22 23:40:48 +0530

    Add existing project files to Git 14

--- src/Topics/PercentageQ.js
+++ src/Topics/PercentageQ.js
@@ -7,11 +7,6 @@ import toast, { Toaster } from 'react-hot-toast';
 
 
 const PercentageQ = () => {
-    const [questions, setQuestions] = useState([]);
-const [searchTerm, setSearchTerm] = useState('');
-const [filteredQuestions, setFilteredQuestions] = useState([]);
-const [userResponses, setUserResponses] = useState({});
-const [mathJaxLoaded, setMathJaxLoaded] = useState(false);
 const [className1, setClassName1] = useState('');
 const [className2, setClassName2] = useState('');
 const [className3, setClassName3] = useState('');
@@ -26,9 +21,7 @@ useEffect(() => {
   script.async = true;
   document.body.appendChild(script);
 
-  script.onload = function () {
-    setMathJaxLoaded(true);
-  };
+  
 
   return () => {
     if (document.body.contains(script)) {
@@ -37,56 +30,18 @@ useEffect(() => {
   };
 }, []);
 
-useEffect(() => {
-  setQuestions(percentageData);
-}, []);
-
-useEffect(() => {
-  setFilteredQuestions(
-    questions.filter((question) =>
-      question.question.toLowerCase().includes(searchTerm.toLowerCase())
-    )
-  );
-  
-}, [questions, searchTerm, mathJaxLoaded]);
 
 
-const handleSearch = (event) => {
-  
-  setSearchTerm(event.target.value);
-  
-};
+
+
+
 
 
 
   
 
-const handleOptionChange = (event, questionIndex) => {
-  const selectedOption = event.target.value;
-  const updatedQuestions = [...questions];
-  const updatedQuestion = { ...updatedQuestions[questionIndex] };
-  updatedQuestion.selectedOption = selectedOption;
-  updatedQuestions[questionIndex] = updatedQuestion;
-  setQuestions(updatedQuestions);
-
-  const updatedResponses = { ...userResponses };
-  updatedResponses[questionIndex] = selectedOption;
-  setUserResponses(updatedResponses);
-};
-
-useEffect(() => {
-  const storedResponses = JSON.parse(localStorage.getItem('userResponses'));
-  if (storedResponses) {
-    setUserResponses(storedResponses);
-  }
-}, []);
 
 
-
-
-useEffect(() => {
-  localStorage.setItem('userResponses', JSON.stringify(userResponses));
-}, [userResponses]);
 const {qid} = useParams()
   return (
     <>
@@ -113,13 +68,6 @@ const {qid} = useParams()
 
 
                            <div className={className1} onClick={()=>{
-                            {/*if(String.fromCharCode(65 + Object.keys(percentageData[qid].options).indexOf(percentageData[qid].answer)) === percentageData[qid].answer){
-                                setClassName1('correct')
-                                toast.success('correct option');
-                            }else{
-                                setClassName1('wrong')
-                                toast.success('wrong option');
-                            }*/}
                             if(String.fromCharCode(65 + 0) === percentageData[qid].answer){
                                 setClassName1('correct')
                                 toast.success('correct option');
@@ -161,6 +109,7 @@ const {qid} = useParams()
                                 </div>              
                                 <div class="questioncontainer answercontainer">  </div>                                
                                 </div>
+                                <Toaster/>
     </>
   )
 }
