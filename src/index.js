4999eced071d0d64b8f5831d4c3a15c959a48edb
Gunjan Jain <jain10gunjan@gmail.com>
2023-04-28 11:26:03 +0530

    template 2 fix

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import Percentage from './Topics/Percentage';
 import PercentageQ from './QuestionsTemplates/PercentageQ';
 import QuadraticEquation from './Topics/QuadraticEquations';
 import QuadraticEquationQ from './QuestionsTemplates/QuadraticEquationQ';
-import Template2 from './Topics/Template2';
 import Numbersystem from './Topics/Numbersystem';
 import Timeanddistance from './Topics/Timeanddistance';
 import NumbersystemQ from './QuestionsTemplates/NumbersystemQ';
@@ -25,7 +24,6 @@ ReactDOM.render(
   <Route path="/number-system/questions/:qid" element={<NumbersystemQ />}/>
   <Route path="/time-and-work" element={<Timeanddistance />}/>
   <Route path="/time-and-work/questions/:qid" element={<TimeandworkQ />}/>
-  <Route path="/test1" element={<Template2 />}/>
 
 
 
